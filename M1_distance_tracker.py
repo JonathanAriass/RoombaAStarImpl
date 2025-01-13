ef65bd855b46a8be43f7302ab6968cb5163f2906
JonathanAriass <UO283586@uniovi.es>
2025-01-13 22:43:06 +0100

    Before start of A* algorithm implementation

--- M1_distance_tracker.py
+++ M1_distance_tracker.py
@@ -1,7 +1,9 @@
-def trackDistance(robot_x, robot_y, origin_x, origin_y):
+def track_distance(robot_x, robot_y, origin_x, origin_y, obstacles, screen_width=800, screen_heigth=800):
     """
         Calculate and return an array of points representing the path of the robot to the origin point.
+        Check colission with obstacles and return the path of the robot to the origin point surrounded by obstacles.
     """
+    # Coords to track path
     coords = []
 
     # Calculate the distance between the robot and the origin point
@@ -24,4 +26,68 @@ def trackDistance(robot_x, robot_y, origin_x, origin_y):
         y = robot_y + i * step_y
         coords.append((x, y))
 
+    # TODO: check colission with obstacles and apply some kind of algorithm to find best path possible
+
     return coords
+
+
+def transform_screen_to_grid(robot_x, robot_y, origin_x, origin_y, obstacles):
+    """
+        Transform the screen into a grid of 800x800 where:
+            - Obstacles (1)
+            - Player (2)
+            - Origin point (3)
+            - Empty space (0)
+    """
+    grid = [[0 for _ in range(800)] for _ in range(800)]
+
+    # Add obstacles to grid
+    for x, y in obstacles:
+        for i in range(x, x + 25):
+            for j in range(y, y + 25):
+                grid[i][j] = 1
+
+    # Add origin point to grid
+    for i in range(int(origin_x) - 10, int(origin_x) + 10):
+        for j in range(int(origin_y) - 10, int(origin_y) + 10):
+            grid[i][j] = 3
+
+    # Add player to grid
+    for i in range(int(robot_x) - 25, int(robot_x) + 25):
+        for j in range(int(robot_y) - 25, int(robot_y) + 25):
+            grid[i][j] = 2
+
+    return grid
+
+
+def update_robot_movement_grid(robot_x, robot_y, grid, previous_robot_coords):
+    """
+        Update the grid when robot moves. So 2's are introduced when robot moves
+        and 0 when robot leaves a space.
+    """
+    # Get previous robot coords
+    x_robot_prev, y_robot_prev = next(iter(previous_robot_coords))
+    
+    # Change 2's to 0's
+    for i in range(x_robot_prev, x_robot_prev + 50):
+        for j in range(y_robot_prev, y_robot_prev + 50):
+            grid[i][j] = 0
+
+    # Update robot position on grid
+    for i in range(int(robot_x), int(robot_x) + 50):
+        for j in range(int(robot_y) - 50, int(robot_y) + 50):
+            grid[i][j] = 2
+
+    # Update previous robot coords
+    new_robot_coords = set()
+    new_robot_coords.add((robot_x, robot_y))
+
+    return grid, new_robot_coords
+
+
+def find_best_path_to_origin_a_star(robot_x, robot_y, origin_x, origin_y, grid,
+                                    robot_width=50, robot_height=50, origin_width=20, origin_height=20):
+    """
+        Find the best path to the origin point using the A* algorithm.
+    """
+    # TODO: implement A* algorithm
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pygame
 import random
-from M1_distance_tracker import trackDistance
+from M1_distance_tracker import track_distance, transform_screen_to_grid, update_robot_movement_grid
+
+
+# TODO: Create a singleton class to store the game data and avoid global variables
+# class GameDataSingleton:
+#     _instance = None
+
+#     def __new__(cls, *args, **kwargs):
+#         if not cls._instance:
+#             cls._instance = super(GameDataSingleton, cls).__new__(cls, *args, **kwargs)
+#             cls._instance.player_x = 0
+#             cls._instance.player_y = 0
+#             cls._instance.origin_x = 0
+#             cls._instance.origin_y = 0
+#             cls._instance.obstacles = []
+#             cls._instance.grid = []
+#         return cls._instance
 
 pygame.init()
 
@@ -23,6 +39,8 @@ player_height = 50
 player_x = (WIDTH - player_width) // 2
 player_y = HEIGHT - player_height - 10
 player_speed = 5
+player_coords = set()
+player_coords.add((player_x, player_y))  # Initial position
 
 # Origin point to track
 origin_x = random.randint(0, WIDTH - player_width)
@@ -30,8 +48,21 @@ origin_y = random.randint(0, HEIGHT - player_height)
 origin_width = 20
 origin_height = 20
 
+# Obstacles properties
+obstacles = []
+obstacle_width = 25
+obstacle_height = 25
+# Make random points
+for i in range(5):
+    x = random.randint(0, WIDTH - obstacle_width)
+    y = random.randint(0, HEIGHT - obstacle_height)
+    obstacles.append((x, y))
+
 # Game loop
 clock = pygame.time.Clock()
+
+# Initialize grid system
+grid = transform_screen_to_grid(player_x, player_y, origin_x, origin_y, obstacles)
 
 running = True
 while running:
@@ -44,28 +75,37 @@ while running:
     keys = pygame.key.get_pressed()
     new_player_x = player_x
     new_player_y = player_y
+    key_pressed = False
 
     if keys[pygame.K_LEFT] and player_x > 0:
         new_player_x -= player_speed
+        key_pressed = True
     if keys[pygame.K_RIGHT] and player_x < WIDTH - player_width:
         new_player_x += player_speed
+        key_pressed = True
     if keys[pygame.K_UP] and player_y > 0:
         new_player_y -= player_speed
+        key_pressed = True
     if keys[pygame.K_DOWN] and player_y < HEIGHT - player_height:
         new_player_y += player_speed
+        key_pressed = True
 
-    # Update player position and check collision
-    player_rect = pygame.Rect(new_player_x, new_player_y, player_width, player_height)
-    origin_rect = pygame.Rect(origin_x, origin_y, origin_width, origin_height)
-    if not player_rect.colliderect(origin_rect):
-        player_x = new_player_x
-        player_y = new_player_y
+    if key_pressed:
+        # Update player position and check collision with origin point and obstacles
+        player_rect = pygame.Rect(new_player_x, new_player_y, player_width, player_height)
+        origin_rect = pygame.Rect(origin_x, origin_y, origin_width, origin_height)
+        obstacles_rect = [pygame.Rect(x, y, obstacle_width, obstacle_height) for x, y in obstacles]
+        if not player_rect.colliderect(origin_rect) and not any(player_rect.colliderect(obstacle) for obstacle in obstacles_rect):
+            player_x = new_player_x
+            player_y = new_player_y
+            # Update robot position on grid
+            grid, player_coords = update_robot_movement_grid(new_player_x, new_player_y, grid, player_coords)
 
     # Screen drawing with white background
     screen.fill(WHITE)
 
     # Calculate the path to the origin point
-    coords = trackDistance(player_x + (player_width / 2 ), player_y + (player_height) / 2, origin_x + ( origin_width / 2), origin_y + (origin_height / 2))
+    coords = track_distance(player_x + (player_width / 2 ), player_y + (player_height) / 2, origin_x + ( origin_width / 2), origin_y + (origin_height / 2), obstacles)
 
     # Draw path
     for x, y in coords:
@@ -79,6 +119,14 @@ while running:
     pygame.draw.rect(screen, BLACK,
                      (origin_x, origin_y, origin_width, origin_height))
 
+    # Draw obstacles
+    for x, y in obstacles:
+        pygame.draw.rect(screen, BLACK, (x, y, obstacle_width, obstacle_height))
+
+    # Draw fps counter on screen
+    font = pygame.font.Font(None, 36)
+    fps = font.render(f"{int(clock.get_fps())} FPS", True, BLACK)
+    screen.blit(fps, (10, 10))
 
     pygame.display.update()
 
